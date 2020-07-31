466ba6e4d32c7030887088930849a17514eea42c
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 18:11:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+#my_collect method
+#take in an argument of a collection
+# iterate over that collection using a while loop
+#execute the code in the block you call it with for each element in the collection (use the yield keyword)
+#return the modified collection
+#list includes the first and last name of each student
+#collect a list of just their first names
+#Should return:["Tim", "Tom", "Jim"]
+#
+#
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+def my_collect(array)
+  my_collect(array) do |name|
+    name.split("").first
+end
 
 
+
+
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
